7b67d3612594ff926d5bbf9d5bc8d3cc9a537901
Vinícius Araújo <viniciusaraujoaj@gmail.com>
2024-06-05 19:15:34 -0300

    Added removeItem and removeItems functionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const form = document.getElementById('form');
 const items = document.querySelector('.list-items');
 const input = document.querySelector('.enter-item');
+const clearAllBtn = document.querySelector('.btn-clear');
 
 // Functions
+
+// AddItem
 const createItem = (name, classes) => {
   const item = document.createElement(name);
   item.className = classes;
@@ -33,5 +36,25 @@ const clearInput = () => {
   input.value = '';
 };
 
+// Remove Item
+const removeItem = (e) => {
+  if (e.target.classList.contains('del-btn')) {
+    e.target.parentElement.remove();
+  }
+};
+
+// Remove Items
+
+const removeAllItems = (e) => {
+  const items = document.querySelectorAll('.item');
+  items.forEach((item) => item.remove());
+
+  //   while (items.firstChild) {
+  //     items.removeChild(items.firstChild);
+  //   }
+};
+
 // Event Listeners
 form.addEventListener('submit', addItem);
+items.addEventListener('click', removeItem);
+clearAllBtn.addEventListener('click', removeAllItems);
